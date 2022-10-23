10f3d2c7deada8e00e143fb9045bc3c79b94d62d
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2022-10-24 00:42:17 +0300

    added_tests_files_&_changed_ci

---
+++ project/tests/test_vector_column.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+#include <vector_column.hpp>
---
+++ project/tests/test_vector_matrix.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <vector_matrix.hpp>
+
+// TEST(vector_matrix, slice) 
+// {
+//     vector_matrix<double, 2, 3> vm = {1, 2, 3, 4, 5, 6};
+//     vector_matrix<double, 1, 2> new_vm = vm.slice<0, 2, 0, 1>();
+//     EXPECT_DOUBLE_EQ(0, 0);
+// }
